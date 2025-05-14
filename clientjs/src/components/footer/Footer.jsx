42737162dbbff0e039edac7d2833273509392e0c
Beatris Ilieve <beatrisilieve@icloud.com>
2025-05-14 21:39:39 +0300

    feat: added Footer component

--- clientjs/src/components/footer/Footer.jsx
+++ clientjs/src/components/footer/Footer.jsx
@@ -1,12 +1,27 @@
-import { Link } from 'react-router'
-import styles from './Footer.module.css'
+import { Link } from 'react-router';
+import styles from './Footer.module.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export const Footer = () => {
     return (
         <footer className={styles['footer']}>
-            <Link to='https://github.com/BeatrisIlieva/drf-react-ts-gems'>
-                <span>https://github.com/BeatrisIlieva/drf-react-ts-gems</span>
+            <h5>connect with us</h5>
+            <Link to='https://github.com/BeatrisIlieva'>
+                <FontAwesomeIcon icon={faGithub} />
+                <span>https://github.com/BeatrisIlieva</span>
             </Link>
+            <Link to='https://www.linkedin.com/in/beatrisilieva'>
+                <FontAwesomeIcon icon={faLinkedin} />
+                <span>https://www.linkedin.com/in/beatrisilieva</span>
+            </Link>
+            <span>
+                <FontAwesomeIcon icon={faEnvelope} />
+                <span>beatris.ilieva@icloud.com</span>
+            </span>
         </footer>
-    )
-}
+    );
+};
