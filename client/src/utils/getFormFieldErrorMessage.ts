861e6811864cf29ad0e7766ca54a654363aa43df
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 22:26:09 +0300

    feat: client  - added getFormFieldErrorMessage util

---
+++ client/src/utils/getFormFieldErrorMessage.ts
@@ -0,0 +1,63 @@
+type ValidatorFn = (value: string) => string;
+
+interface ValidatorType {
+    [key: string]: {
+        [key: string]: ValidatorFn;
+    };
+}
+
+const passwordErrorMessage =
+    'Sorry, the provided password does not match the required constraints';
+
+const emailErrorMessage = 'Please enter a valid email address';
+
+export const validators: ValidatorType = {
+    email: {
+        isValid: (value) =>
+            /^[A-Za-z0-9]+@+[a-z]+\.[a-z]{2,4}$/.test(value)
+                ? ''
+                : emailErrorMessage
+    },
+    password: {
+        length: (password) =>
+            password.length >= 6 ? '' : passwordErrorMessage,
+
+        upper: (password) =>
+            /[A-Z]/.test(password) ? '' : passwordErrorMessage,
+
+        lower: (password) =>
+            /[a-z]/.test(password) ? '' : passwordErrorMessage,
+
+        number: (password) => (/\d/.test(password) ? '' : passwordErrorMessage),
+
+        noSpaces: (password) =>
+            !/\s/.test(password) ? '' : passwordErrorMessage,
+
+        special: (password) =>
+            /[!#$%]/.test(password) ? '' : passwordErrorMessage,
+    }
+};
+
+export const getFormFieldErrorMessage = (field: string, value: string): string => {
+    const emptyErrorMessage = 'This field is required';
+
+    if (value === '') {
+        return emptyErrorMessage;
+    }
+
+    if (!validators[field]) {
+        return '';  // If no validators for this field, consider valid
+    }
+
+    let error = '';
+
+    for (const validationFn of Object.values(validators[field])) {
+        error = validationFn(value);
+
+        if (error !== '') {
+            return error;
+        }
+    }
+
+    return error;
+};
