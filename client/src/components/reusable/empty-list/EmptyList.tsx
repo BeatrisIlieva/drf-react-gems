a9704542cc6cdf717def747aba3810ac11f958ae
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 22:24:07 +0300

    feat: client - cerated reusable EmptyList component

---
+++ client/src/components/reusable/empty-list/EmptyList.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement } from 'react';
+import { ShopByCategory } from '../../common/shop-by-category/ShopByCategory';
+
+import styles from './EmptyList.module.scss';
+
+interface Props {
+    title: string;
+}
+
+export const EmptyList = ({ title }: Props): ReactElement => {
+    return (
+        <div className={styles['empty-bag-container']}>
+            <div className={styles['empty-bag-message']}>
+                <h3>Your {title} is empty</h3>
+                <p>
+                    Browse our categories below to discover
+                    beautiful pieces for your collection
+                </p>
+            </div>
+            <ShopByCategory />
+        </div>
+    );
+};
