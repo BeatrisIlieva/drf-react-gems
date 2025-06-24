fb0b555d4954ac2f740c1b3990ce55a84068a08d
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 21:47:55 +0300

    chore: client - removed LoadingSpinner

--- client/src/components/common/loading-spinner/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './LoadingSpinner.module.scss';
-
-export const LoadingSpinner = () => {
-    return (
-        <div className={styles['loading-spinner']}>
-            <p>Loading</p>
-        </div>
-    )
-}
