eaa35e85d335181055e4362ac2a24be5b62ac475
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 21:52:49 +0300

    fix: client fixed formatPrice util

--- client/src/utils/formatPrice.ts
+++ client/src/utils/formatPrice.ts
@@ -1,6 +1,9 @@
 export const formatPrice = (price: string): string => {
     const splittedPrice = price.split('.')[0];
-    const start = splittedPrice.slice(0, splittedPrice.length - 3);
+    const start = splittedPrice.slice(
+        0,
+        splittedPrice.length - 3
+    );
     const middle = ',';
     const end = splittedPrice.slice(start.length);
     const formattedPrice = `$${start}${middle}${end}`;
