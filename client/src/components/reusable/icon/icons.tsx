734cc87d2d335904db7d0bc489fe2bce9407c3c2
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 22:17:14 +0300

    feat: client - added more icons into the icons.tsx

--- client/src/components/reusable/icon/icons.tsx
+++ client/src/components/reusable/icon/icons.tsx
@@ -93,6 +93,21 @@ export const icons: Record<string, ReactElement> = {
             />
         </svg>
     ),
+    arrowLeft: (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+        >
+            <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18'
+            />
+        </svg>
+    ),
     xMark: (
         <svg
             xmlns='http://www.w3.org/2000/svg'
