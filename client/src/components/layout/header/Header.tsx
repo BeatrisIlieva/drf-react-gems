37af6ef947b4df6b038b6050fd99af5e41039d5c
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 21:58:51 +0300

    chore: client - added scss animations into Header component

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -19,7 +19,10 @@ export const Header = (): ReactElement => {
             const header = headerRef.current;
             if (!header) return;
 
-            if (currentScrollY > 0 && currentScrollY > lastScrollY) {
+            if (
+                currentScrollY > 0 &&
+                currentScrollY > lastScrollY
+            ) {
                 header.classList.remove(styles.visible);
                 header.classList.add(styles.hidden);
             } else if (currentScrollY < lastScrollY) {
@@ -31,13 +34,17 @@ export const Header = (): ReactElement => {
         };
 
         window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+        return () =>
+            window.removeEventListener('scroll', handleScroll);
     }, []);
 
     return (
         <>
-        <Banner/>
-            <div ref={sentinelRef} className={styles['sentinel']} />
+            <Banner />
+            <div
+                ref={sentinelRef}
+                className={styles['sentinel']}
+            />
 
             <header
                 ref={headerRef}
