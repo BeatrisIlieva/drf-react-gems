9a70758615dd5c41ef2cd0cdf360cb880e43bf00
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 22:19:12 +0300

    feat: client  - added Logout component

---
+++ client/src/components/accounts/logout/Logout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useUserContext from '../../../contexts/UserContext';
+import { useLogout } from '../../../api/authApi';
+
+export const Logout: React.FC = () => {
+    const { logout } = useLogout();
+    const { userLogoutHandler } = useUserContext();
+
+    const logoutHandler = async () => {
+        try {
+            await logout();
+            userLogoutHandler();
+        } catch (err) {
+            console.log(err instanceof Error ? err.message : String(err));
+        }
+    };
+
+    return <button onClick={logoutHandler}>Sign out</button>;
+};
