a17bb0d7be77abebed4ddcdd74b7ed53054568f3
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 22:15:05 +0300

    chore: client - moved ShoppingBagList skeleton into separate file

--- client/src/components/pages/shopping-bag/shopping-bag-list/ShoppingBagList.tsx
+++ client/src/components/pages/shopping-bag/shopping-bag-list/ShoppingBagList.tsx
@@ -2,13 +2,15 @@ import { useEffect, type ReactElement } from 'react';
 import { useShoppingBagContext } from '../../../../contexts/ShoppingBagContext';
 import styles from './ShoppingBagList.module.scss';
 import { ShoppingBagItem } from './shopping-bag-item/ShoppingBagItem';
+import { Skeleton } from './skeleton/Skeleton';
 
 export const ShoppingBagList = (): ReactElement => {
     const {
         shoppingBagItems,
         getShoppingBagItemsHandler,
         updateShoppingBagCount,
-        updateShoppingBagTotalPrice
+        updateShoppingBagTotalPrice,
+        isLoading
     } = useShoppingBagContext();
 
     useEffect(() => {
@@ -20,6 +22,13 @@ export const ShoppingBagList = (): ReactElement => {
         updateShoppingBagTotalPrice,
         getShoppingBagItemsHandler
     ]);
+
+    if (
+        isLoading &&
+        (!shoppingBagItems || shoppingBagItems.length === 0)
+    ) {
+        return <Skeleton />;
+    }
 
     return (
         <ul className={styles['shopping-bag-list']}>
