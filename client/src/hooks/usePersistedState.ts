d9efa9d7db1d6bdb2f01cd769a2d410a3f5968bc
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 22:05:05 +0300

    chore: client - modified usePersistedState

--- client/src/hooks/usePersistedState.ts
+++ client/src/hooks/usePersistedState.ts
@@ -1,39 +1,35 @@
-import { useCallback, useState } from 'react';
-import type { Dispatch, SetStateAction } from 'react';
+import { useCallback, useState } from "react";
 
-export const usePersistedState = <T>(
-    stateKey: string,
-    initialState: T | (() => T)
-): [T, Dispatch<SetStateAction<T>>] => {
+type SetStateAction<T> = T | ((prevState: T) => T);
+
+export default function usePersistedState<T>(stateKey: string, initialState: T | (() => T)): [T, (value: SetStateAction<T>) => void] {
     const [state, setState] = useState<T>(() => {
         const persistedState = localStorage.getItem(stateKey);
         if (!persistedState) {
-            return typeof initialState === 'function'
-                ? (initialState as () => T)()
+            return typeof initialState === 'function' 
+                ? (initialState as () => T)() 
                 : initialState;
         }
 
-        try {
-            return JSON.parse(persistedState) as T;
-        } catch {
-            return typeof initialState === 'function'
-                ? (initialState as () => T)()
-                : initialState;
-        }
+        const persistedStateData = JSON.parse(persistedState);
+
+        return persistedStateData as T;
     });
 
-    const setPersistedState: Dispatch<SetStateAction<T>> = useCallback(
-        (input) => {
-            const data =
-                typeof input === 'function'
-                    ? (input as (prev: T) => T)(state)
-                    : input;
+    const setPersistedState = useCallback((input: SetStateAction<T>) => {
+        const data = typeof input === 'function' 
+            ? (input as (prevState: T) => T)(state) 
+            : input;
 
-            localStorage.setItem(stateKey, JSON.stringify(data));
-            setState(data);
-        },
-        [state, stateKey]
-    );
+        const persistedData = JSON.stringify(data);
 
-    return [state, setPersistedState];
-};
+        localStorage.setItem(stateKey, persistedData);
+
+        setState(data);
+    }, [state, stateKey]);
+
+    return [
+        state,
+        setPersistedState,
+    ]
+}
