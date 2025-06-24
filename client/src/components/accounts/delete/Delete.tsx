e35a42e968e6ce3f02f977a601dd47b696132cb4
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-24 22:18:13 +0300

    feat: client  - added Delete accounts component

---
+++ client/src/components/accounts/delete/Delete.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import useUserContext from '../../../contexts/UserContext';
+import { useDelete } from '../../../api/authApi';
+
+export const Delete: React.FC = () => {
+    const { deleteUser } = useDelete();
+    const { userLogoutHandler } = useUserContext();
+
+    const deleteHandler = async () => {
+        try {
+            await deleteUser();
+            userLogoutHandler();
+        } catch (err) {
+            console.log(err instanceof Error ? err.message : String(err));
+        }
+    };
+
+    return (
+        <button type='submit' onClick={deleteHandler}>
+            Delete
+        </button>
+    );
+};
