094104fa80fe236596dc905c114b6b0504a1047f
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-27 13:23:03 +0300

    fix: server - refactored accounts validators to include the method get_error_message

--- server/src/accounts/validators/password.py
+++ server/src/accounts/validators/password.py
@@ -7,9 +7,12 @@ class DigitRequiredValidator:
     def validate(self, password, user=None):
         if not any(char.isdigit() for char in password):
             raise ValidationError(
-                _("Your password must contain at least one digit."),
+                self.get_error_message(),
                 code='password_no_digit',
             )
+
+    def get_error_message(self):
+        return _("Your password must contain at least one digit.")
 
     def get_help_text(self):
         return _("Your password must contain at least one digit.")
@@ -19,9 +22,12 @@ class UpperCaseLetterRequiredValidator:
     def validate(self, password, user=None):
         if not any(char.isupper() for char in password):
             raise ValidationError(
-                _("Your password must contain at least one upper case letter."),
+                self.get_error_message(),
                 code='password_no_upper_case_letter',
             )
+
+    def get_error_message(self):
+        return _("Your password must contain at least one upper case letter.")
 
     def get_help_text(self):
         return _("Your password must contain at least one upper case letter.")
@@ -31,9 +37,12 @@ class LowerCaseLetterRequiredValidator:
     def validate(self, password, user=None):
         if not any(char.islower() for char in password):
             raise ValidationError(
-                _("Your password must contain at least one lower case letter."),
+                self.get_error_message(),
                 code='password_no_lower_case_letter',
             )
+
+    def get_error_message(self):
+        return _("Your password must contain at least one lower case letter.")
 
     def get_help_text(self):
         return _("Your password must contain at least one lower case letter.")
@@ -43,9 +52,12 @@ class NoWhiteSpacesRequiredValidator:
     def validate(self, password, user=None):
         if any(char.isspace() for char in password):
             raise ValidationError(
-                _("Your password must not contain white spaces."),
+                self.get_error_message(),
                 code='password_no_white_spaces',
             )
+
+    def get_error_message(self):
+        return _("Your password must not contain white spaces.")
 
     def get_help_text(self):
         return _("Your password must not contain white spaces.")
@@ -56,11 +68,12 @@ class SpecialCharRequiredValidator:
         special_chars = '!#$%'
         if not any(char in special_chars for char in password):
             raise ValidationError(
-                _("Your password must contain at least one special character (!#$%)"),
+                self.get_error_message(),
                 code='password_no_special_char',
             )
 
-    def get_help_text(self):
+    def get_error_message(self):
         return _("Your password must contain at least one special character (!#$%)")
 
-
+    def get_help_text(self):
+        return _("Your password must contain at least one special character (!#$%)")
