b7b66a69f62a81b59cb1b01f66c69bed7d13d37a
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-30 22:37:06 +0300

    feat: client - added hide/unhide password functionality

--- clientjs/src/components/reusable/input-field/InputField.jsx
+++ clientjs/src/components/reusable/input-field/InputField.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faEye,
+    faEyeSlash
+} from '@fortawesome/free-solid-svg-icons';
+import styles from './InputField.module.scss';
+
 export const InputField = ({
     getInputClassName,
     fieldData,
     validateField,
     fieldName,
-    type,
+    type
 }) => {
-
-    
+    const [isPasswordVisible, setIsPasswordVisible] =
+        useState(false);
     return (
         <div className='field'>
             <input
                 className={getInputClassName(fieldData)}
-                type={type}
+                type={
+                    type === 'text' || !isPasswordVisible
+                        ? type
+                        : 'password'
+                }
                 name={fieldName}
                 id={fieldName}
                 placeholder={fieldName}
@@ -23,8 +36,27 @@ export const InputField = ({
                 htmlFor={fieldName}
                 className={getInputClassName(fieldData)}
             >
-                {`${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}*`}
+                {`${
+                    fieldName.charAt(0).toUpperCase() +
+                    fieldName.slice(1)
+                }*`}
             </label>
+            <i
+                onClick={setIsPasswordVisible((prev) => !prev)}
+                className={
+                    isPasswordVisible ? (
+                        <FontAwesomeIcon
+                            icon={faEyeSlash}
+                            className={styles['eye-icon']}
+                        />
+                    ) : (
+                        <FontAwesomeIcon
+                            icon={faEye}
+                            className={styles['eye-icon']}
+                        />
+                    )
+                }
+            ></i>
             {fieldData.error && (
                 <span className='error'>{fieldData.error}</span>
             )}
