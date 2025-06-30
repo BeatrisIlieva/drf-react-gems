00bfbd8219dd0fedd243c257be1ee4be6b64a713
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-30 22:47:57 +0300

    chore: client - updated Details component margins

--- client/src/components/pages/accounts/details/Details.tsx
+++ client/src/components/pages/accounts/details/Details.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { ProfilePhotoUpload } from './profile-photo-upload/ProfilePhotoUpload';
 import { usePhotoManager } from '../../../../hooks/usePhotoManager';
-import styles from './Details.module.scss';
 import { PersonalInfoForm } from './personal-info-form/PersonalInfoForm';
-
+import { LoginInformation } from './login-information/LoginInformation';
+import styles from './Details.module.scss';
 
 export const Details: React.FC = () => {
-    const { currentPhoto, isLoading, isUploading, uploadPhoto } =
+    const { currentPhoto, isUploading, uploadPhoto } =
         usePhotoManager();
 
     return (
         <section className={styles['details']}>
             <section className={styles['photo']}>
-                <h4>
-                    Profile Photo
-                </h4>
+                <h4>Profile Photo</h4>
 
                 <ProfilePhotoUpload
                     currentPhoto={currentPhoto}
@@ -22,7 +20,9 @@ export const Details: React.FC = () => {
                     isUploading={isUploading}
                 />
             </section>
+            <h2>Account Details</h2>
             <PersonalInfoForm />
+            <LoginInformation />
         </section>
     );
 };
