a9c76c4dc6da693370669f33d0b07bd432028181
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-04 22:13:53 +0300

    chore: client - added more data into the formHelpers util

--- client/src/utils/formHelpers.js
+++ client/src/utils/formHelpers.js
@@ -6,22 +6,34 @@ export const createFormFieldConfig = (fields) => {
             required: field.required !== false,
             type: field.type || 'text',
             label: field.label,
-            validation: field.validation || []
+            validation: field.validation || [],
+            maxLength:
+                field.maxLength ||
+                getDefaultMaxLength(field.name, field.type)
         };
         return config;
     }, {});
 };
 
-export const getInitialFormValues = (fieldNames) => {
+export const getInitialFormValues = (
+    fieldNames,
+    fieldConfig = {}
+) => {
     return fieldNames.reduce((values, fieldName) => {
-        values[fieldName] = { value: '', error: '', valid: false };
+        const config = fieldConfig[fieldName] || {};
+        values[fieldName] = {
+            value: '',
+            error: '',
+            valid: false,
+            maxLength: config.maxLength
+        };
         return values;
     }, {});
 };
 
 export const createApiDataFromForm = (formData, fieldConfig) => {
     const apiData = {};
-    Object.keys(formData).forEach(fieldName => {
+    Object.keys(formData).forEach((fieldName) => {
         const field = fieldConfig[fieldName];
         const value = formData[fieldName]?.value?.trim();
         if (value && field) {
@@ -29,4 +41,32 @@ export const createApiDataFromForm = (formData, fieldConfig) => {
         }
     });
     return apiData;
+};
+
+export const getDefaultMaxLength = (fieldName, fieldType) => {
+    const defaultLengths = {
+        email: 254,
+        email_or_username: 254,
+        username: 30,
+        firstName: 30,
+        lastName: 30,
+        phoneNumber: 15,
+        password: 128,
+        currentPassword: 128,
+        newPassword: 128,
+        streetAddress: 100,
+        apartment: 50,
+        city: 50,
+        state: 50,
+        zipCode: 10,
+        cardNumber: 19,
+        cardHolderName: 50,
+        cvv: 4
+    };
+
+    if (fieldType === 'password') return 128;
+    if (fieldType === 'email') return 254;
+    if (fieldType === 'tel') return 15;
+
+    return defaultLengths[fieldName] || 100;
 };
