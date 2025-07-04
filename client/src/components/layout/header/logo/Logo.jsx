4b7627ea9fe09c6e4cac18f43411e946828b086a
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-04 22:11:38 +0300

    chore: client - renamed the logo

--- client/src/components/layout/header/logo/Logo.jsx
+++ client/src/components/layout/header/logo/Logo.jsx
@@ -5,7 +5,7 @@ import styles from './Logo.module.scss';
 export const Logo = () => {
     return (
         <Link to='/' className={styles['logo']}>
-            <span>DRF React TS Gems</span>
+            <span>DRF React Gems</span>
         </Link>
     );
 };
