cf4b35dc65786e3595ffcd835e6ca3a4b454a71c
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-05 22:20:36 +0300

    feat: client - added PaddedContainer component

---
+++ client/src/components/reusable/padded-container/PaddedContainer.jsx
@@ -0,0 +1,27 @@
+import styles from './PaddedContainer.module.scss';
+
+export const PaddedContainer = ({
+    children,
+    backgroundColor
+}) => {
+    const getBackgroundClass = (bgColor) => {
+        const colorMap = {
+            white: styles['padded-container--white'],
+            'lightest-grey':
+                styles['padded-container--lightest-grey']
+        };
+        return colorMap[bgColor] || '';
+    };
+
+    const backgroundClass = backgroundColor
+        ? getBackgroundClass(backgroundColor)
+        : '';
+    const containerClasses = [
+        styles['padded-container'],
+        backgroundClass
+    ]
+        .filter(Boolean)
+        .join(' ');
+
+    return <div className={containerClasses}>{children}</div>;
+};
