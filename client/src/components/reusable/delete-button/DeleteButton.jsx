d4b4da9be5bdd08810682404ee29e382335e6c2b
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-08 22:43:34 +0300

    feat: client - added DeleteButton component

---
+++ client/src/components/reusable/delete-button/DeleteButton.jsx
@@ -0,0 +1,12 @@
+import styles from './DeleteButton.module.scss';
+
+export const DeleteButton = ({ entityName, callbackHandler }) => {
+    return (
+        <button
+            className={styles['delete-button']}
+            onClick={callbackHandler}
+        >
+            Delete {entityName}
+        </button>
+    );
+};
