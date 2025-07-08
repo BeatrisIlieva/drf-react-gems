0134730b1a0581ae916e1251fb947efa260c24a4
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-08 22:22:28 +0300

    chore: client - added FormFieldRenderer its own directory after moving it into the reusable directory

---
+++ client/src/components/reusable/form-field-renderer/FormFieldRenderer.jsx
@@ -0,0 +1,34 @@
+import { Fragment } from 'react';
+import { InputField } from '../input-field/InputField';
+
+export const FormFieldRenderer = ({
+    fieldNames,
+    formData,
+    getInputClassName,
+    handleFieldChange,
+    validateField,
+    registerInput,
+    fieldConfig = {}
+}) => {
+    return fieldNames.map((fieldName) => {
+        const fieldData = formData[fieldName];
+        const config = fieldConfig[fieldName] || {};
+
+        if (!fieldData) return null;
+
+        return (
+            <Fragment key={fieldName}>
+                <InputField
+                    getInputClassName={getInputClassName}
+                    fieldData={fieldData}
+                    handleFieldChange={handleFieldChange}
+                    validateField={validateField}
+                    fieldName={fieldName}
+                    type={config.type || 'text'}
+                    registerInput={registerInput}
+                    fieldConfig={fieldConfig}
+                />
+            </Fragment>
+        );
+    });
+};
