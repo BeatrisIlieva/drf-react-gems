20b4bd5ea040cbd7751b3d0a858867b153f4f10e
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-08 22:32:43 +0300

    chore: client added margin bottom to the ProductItems component

--- client/src/components/reusable/product-items/ProductItems.jsx
+++ client/src/components/reusable/product-items/ProductItems.jsx
@@ -3,7 +3,6 @@ import styles from './ProductItems.module.scss';
 import { ProductCard } from './product-card/ProductCard';
 
 export const ProductItems = ({ products }) => {
-
     return (
         <ul className={styles['products']}>
             {products?.map((product) => (
