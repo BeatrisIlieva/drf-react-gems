4991b3bb6b29a1adf82f8451bd7f82658cccd007
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-08 22:43:54 +0300

    feat: client - added Deletion components

---
+++ client/src/components/reusable/deletion/Deletion.jsx
@@ -0,0 +1,29 @@
+import { Button } from '../button/Button';
+import styles from './Deletion.module.scss';
+
+export const Deletion = ({ entityName, onProceed, onCancel }) => {
+    return (
+        <section className={styles['deletion']}>
+            <h3>Delete {entityName}</h3>
+
+            <p>
+                Are you sure you want to delete your {entityName}?
+            </p>
+
+            <div className={styles['buttons-wrapper']}>
+                <Button
+                    title='Yes, Delete'
+                    buttonGrow='1'
+                    color='black'
+                    callbackHandler={onProceed}
+                />
+                <Button
+                    title='Cancel'
+                    buttonGrow='1'
+                    color='white'
+                    callbackHandler={onCancel}
+                />
+            </div>
+        </section>
+    );
+};
