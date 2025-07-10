43e83696bf068c7c4188814a6bf0e97102d38d5f
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-10 21:43:52 +0300

    chore: client - added higher padding-inline to the ShoppingBag component when in desktop mode

--- client/src/components/pages/shopping-bag/shopping-bag-list/ShoppingBagList.jsx
+++ client/src/components/pages/shopping-bag/shopping-bag-list/ShoppingBagList.jsx
@@ -1,17 +1,12 @@
 import { ShadowBox } from '../../../reusable/shadow-box/ShadowBox';
 import { ShoppingBagItem } from './shopping-bag-item/ShoppingBagItem';
-import { Skeleton } from './skeleton/Skeleton';
 
 import { useShoppingBagContext } from '../../../../contexts/ShoppingBagContext';
 
 import styles from './ShoppingBagList.module.scss';
 
 export const ShoppingBagList = () => {
-    const { shoppingBagItems, loading } = useShoppingBagContext();
-
-    if (loading && (!shoppingBagItems || shoppingBagItems.length === 0)) {
-        return <Skeleton />;
-    }
+    const { shoppingBagItems } = useShoppingBagContext();
 
     return (
         <ShadowBox>
