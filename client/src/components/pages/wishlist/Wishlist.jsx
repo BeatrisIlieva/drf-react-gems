0b0c697826a3eeb037b0baecabac48e6a1d07366
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-11 16:58:31 +0300

    chore: client - added items count into the heading of the Wishlist component

--- client/src/components/pages/wishlist/Wishlist.jsx
+++ client/src/components/pages/wishlist/Wishlist.jsx
@@ -6,7 +6,7 @@ import { useWishlistContext } from '../../../contexts/WishlistContext';
 import styles from './Wishlist.module.scss';
 
 export const Wishlist = () => {
-    const { wishlistItems, loading } = useWishlistContext();
+    const { wishlistItems, loading, wishlistItemsCount } = useWishlistContext();
 
     return (
         <>
@@ -14,7 +14,12 @@ export const Wishlist = () => {
                 <>
                     {wishlistItems.length > 0 ? (
                         <section className={styles['wishlist']}>
-                            <h2>Your Wishlist</h2>
+                            <div className={styles['heading-wrapper']}>
+                                <h2>Your Wishlist</h2>
+                                <span>
+                                    {wishlistItemsCount} {wishlistItemsCount > 1 ? 'items' : 'item'}
+                                </span>
+                            </div>
                             <ProductItems products={wishlistItems} />
                         </section>
                     ) : (
