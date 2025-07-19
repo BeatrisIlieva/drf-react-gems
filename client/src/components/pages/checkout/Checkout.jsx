0c1002cab21abe16a89b4852f31867d3bb142d4e
Beatris Ilieve <beatrisilieve@icloud.com>
2025-07-19 19:22:47 +0300

    chore: client - moved the submit button of the ShippingDetailsForm inside the form itself since it was placed outside when the location is checkout

--- client/src/components/pages/checkout/Checkout.jsx
+++ client/src/components/pages/checkout/Checkout.jsx
@@ -1,8 +1,9 @@
-import { useCallback, useEffect, useState, useTransition } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useNavigate } from 'react-router';
 
 import { Button } from '../../reusable/button/Button';
+import { CheckoutHeader } from '../../reusable/checkout-header/CheckoutHeader';
 import { ComplimentaryShipping } from '../../reusable/complimentary-shipping/ComplimentaryShipping';
 import { Delivery } from '../../reusable/delivery/Delivery';
 import { OrderSummary } from '../../reusable/order-summary/OrderSummary';
@@ -16,25 +17,14 @@ import { useShoppingBagContext } from '../../../contexts/ShoppingBagContext';
 import styles from './Checkout.module.scss';
 
 export const Checkout = () => {
-    const [isPending, startTransition] = useTransition();
     const navigate = useNavigate();
     const { shoppingBagItems } = useShoppingBagContext();
 
-    const [submitAction, setSubmitAction] = useState(null);
     const [formState, setFormState] = useState(null);
 
-    const handleFormReady = ({ submitAction, formState }) => {
-        setSubmitAction(() => submitAction);
+    const handleFormReady = ({ formState }) => {
         setFormState(formState);
     };
-
-    const continueCheckoutHandler = useCallback(() => {
-        if (submitAction) {
-            startTransition(() => {
-                submitAction();
-            });
-        }
-    }, [submitAction, startTransition]);
 
     useEffect(() => {
         if (formState?.success) {
@@ -44,31 +34,20 @@ export const Checkout = () => {
 
     return (
         <PaddedContainer backgroundColor="lightest-grey">
+            <CheckoutHeader title="Checkout" activePath="Shipping" inactivePath="Payment" />
             <section className={styles['checkout']}>
                 <div className={styles['wrapper-left']}>
-                    <DeliveryAddressForm
-                        buttonTitle="Continue Checkout"
-                        buttonGrow="1"
-                        showButton={false}
-                        onFormReady={handleFormReady}
-                    />
                     <ShadowBox>
                         <ProductsSummaryList products={shoppingBagItems}>
                             <Delivery fontSize={1.2} />
                         </ProductsSummaryList>
                     </ShadowBox>
-                    <div className={styles['checkout-button-container']}>
-                        <ShadowBox>
-                            <Button
-                                title="Continue Checkout"
-                                color="black"
-                                actionType="button"
-                                pending={isPending}
-                                callbackHandler={continueCheckoutHandler}
-                                buttonGrow="1"
-                            />
-                        </ShadowBox>
-                    </div>
+
+                    <DeliveryAddressForm
+                        buttonTitle="Continue Checkout"
+                        buttonGrow="1"
+                        onFormReady={handleFormReady}
+                    />
                 </div>
 
                 <OrderSummary separate={true}>
