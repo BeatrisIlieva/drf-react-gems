f50511b7eb9c40bba6fa5d7b8d578392ccb085fc
Beatris Ilieve <beatrisilieve@icloud.com>
2025-09-05 22:37:50 +0300

    fix: client - removed infinite scroll and replaced with load more button due to duplicate requests for one and the same page

--- client/src/components/pages/product-list/ProductList.jsx
+++ client/src/components/pages/product-list/ProductList.jsx
@@ -1,6 +1,5 @@
-import { useCallback, useEffect } from 'react';
-
 import { LoadingSpinner } from '../../common/loading-spinner/LoadingSpinner';
+import { Button } from '../../reusable/button/Button';
 import { ProductItems } from '../../reusable/product-items/ProductItems';
 import { FilterList } from './filter-list/FilterList';
 import { HomeLink } from './home-link/HomeLink';
@@ -13,38 +12,11 @@ import { useProductListContext } from '../../../contexts/ProductListContext';
 
 import styles from './ProductList.module.scss';
 
-const SCROLL_OFFSET = 10;
-
-function debounce(fn, delay) {
-    let timeoutId;
-    return () => {
-        clearTimeout(timeoutId);
-        timeoutId = window.setTimeout(fn, delay);
-    };
-}
-
 export const ProductList = () => {
     const { products, loading, loadMoreHandler, loadMoreDisabled } = useProductListContext();
     const { displayFilters } = useProductFiltersContext();
 
     const { sentinelRef, isSticky } = useSentinel();
-
-    const handleScroll = useCallback(() => {
-        const scrollPosition = window.innerHeight + window.scrollY;
-        const bottomPosition = document.documentElement.scrollHeight;
-
-        const nearBottom = bottomPosition - scrollPosition < SCROLL_OFFSET;
-
-        if (nearBottom && !loading && !loadMoreDisabled) {
-            loadMoreHandler();
-        }
-    }, [loading, loadMoreDisabled, loadMoreHandler]);
-
-    useEffect(() => {
-        const debouncedScroll = debounce(handleScroll, 150);
-        window.addEventListener('scroll', debouncedScroll);
-        return () => window.removeEventListener('scroll', debouncedScroll);
-    }, [handleScroll]);
 
     return (
         <>
@@ -70,6 +42,15 @@ export const ProductList = () => {
                         {products.length > 0 && <ProductItems products={products} />}
                     </div>
                 </div>
+                {!loadMoreDisabled && (
+                    <Button
+                        color="black"
+                        title="Load more"
+                        buttonGrow="1"
+                        width="10"
+                        callbackHandler={loadMoreHandler}
+                    />
+                )}
             </section>
         </>
     );
