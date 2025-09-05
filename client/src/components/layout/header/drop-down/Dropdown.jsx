474cb3e4b180571f6a0ec27fa1881c37002d369b
Beatris Ilieve <beatrisilieve@icloud.com>
2025-09-05 15:41:08 +0300

    fix: client - the navigation used to not close on x-mark click in responsive mode

--- client/src/components/layout/header/drop-down/Dropdown.jsx
+++ client/src/components/layout/header/drop-down/Dropdown.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react';
-
 import { Nav } from '../../../reusable/nav/Nav';
 
 import { navLinks } from '../../../../constants/mainNavLinksData';
@@ -7,20 +5,8 @@ import { navLinks } from '../../../../constants/mainNavLinksData';
 import styles from './Dropdown.module.scss';
 
 export const Dropdown = ({ onClose }) => {
-    const dropdownRef = useRef(null);
-
-    useEffect(() => {
-        const handleClick = e => {
-            if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
-                onClose();
-            }
-        };
-        document.addEventListener('mousedown', handleClick);
-        return () => document.removeEventListener('mousedown', handleClick);
-    }, [onClose]);
-
     return (
-        <div ref={dropdownRef} className={styles['dropdown']}>
+        <div className={styles['dropdown']}>
             <Nav links={navLinks} flexDirection="column" onLinkClick={onClose} />
         </div>
     );
